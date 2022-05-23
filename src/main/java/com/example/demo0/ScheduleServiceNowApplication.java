c524d27400dc95e98691ca62a5d09be2536a066a
Taime Nunes <taimenunes@hotmail.com>
2022-05-23 20:38:08 -0300

    Projeto Criado

---
+++ src/main/java/com/example/demo0/ScheduleServiceNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServiceNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServiceNowApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo0/controller/HelloController.java
@@ -0,0 +1,5 @@
+package com.example.demo0.controller;
+
+public class HelloController {
+
+}
